92c1cdea68adea81f9bd29720fc538f20e993026
Matheus Lauffer <matheuslauffer1@gmail.com>
2017-10-27 15:02:52 -0200

    Página inicial

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,8 @@
 module.exports = function(application){
 	application.get('/', function(req, res){
-		res.send('Bem vindo a sua app NodeJS!');
+		res.render('index');
+	});
+	application.post('/inicial', function(req, res){
+		res.render('inicial');
 	});
 }
