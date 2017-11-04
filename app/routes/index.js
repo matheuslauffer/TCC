36c218bf27d5307d3d9a27e4150726b066009805
Matheus Lauffer <matheuslauffer1@gmail.com>
2017-11-04 19:17:12 -0200

    correções

--- app/routes/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(application){
-	application.get('/', function(req, res){
-		res.render('index');
-	});
-	application.post('/inicial', function(req, res){
-		res.render('inicial');
-	});
-}
