81049b2db37e03307ee8912befdb050211facf67
Matheus Lauffer <matheuslauffer1@gmail.com>
2017-11-04 20:00:44 -0200

    Alteração no estilo

--- client/app/public/lib/style.js
+++ client/app/public/lib/style.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
   $("input[type=text]").focusin(function() {
-    //$(this).css("border-color","#FFFFFF");
     $(".label").css("color", "#FFFFFF");
   });
   $("input[type=password]").focusin(function() {
-    //$(this).css("border-color","#FFFFFF");
     $(".label").css("color", "#FFFFFF");
   });
 });
