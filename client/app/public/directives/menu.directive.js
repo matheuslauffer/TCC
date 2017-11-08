89409e96ecf01cfa592c90629344de6ea1bbdf2a
Matheus Lauffer <matheuslauffer1@gmail.com>
2017-11-08 14:47:39 -0200

    inclusão do menu no single page

---
+++ client/app/public/directives/menu.directive.js
@@ -0,0 +1,6 @@
+angular.module("BoraJogar").directive("uiMenu", function(){
+  return{
+    templateUrl: "/views/menu.html",
+    restrict: "E"
+  };
+});
--- client/app/public/lib/style.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("input[type=text]").focusin(function() {
-    $(".label").css("color", "#FFFFFF");
-  });
-  $("input[type=password]").focusin(function() {
-    $(".label").css("color", "#FFFFFF");
-  });
-});
