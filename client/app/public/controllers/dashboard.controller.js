5cacf6d1e0b81c3522e4adb4daababe380e5f28d
Matheus Lauffer <matheuslauffer1@gmail.com>
2018-02-03 13:53:13 -0200

    Finalização do dashboard

--- client/app/public/controllers/dashboard.controller.js
+++ client/app/public/controllers/dashboard.controller.js
@@ -1,4 +1,15 @@
-angular.module("BoraJogar").controller('dashboardController', function($scope, $location, $localStorage){
+angular.module("BoraJogar").controller('dashboardController', function($scope, $location, $localStorage, matchAPI){
+    matchAPI.getMyMatchs($localStorage.user).then(function(res){
+      $scope.nextMatch = res.data[0];
+    }).catch(function(error){
+      console.log(error);
+    });
+
+    matchAPI.getMyInvites($localStorage.user).then(function(res){
+      $scope.invites = res.data;
+    }).catch(function(error){
+      console.log(error);
+    });
 
   $scope.redirectCreateMatch = function() {
     $location.path('/create-match');
