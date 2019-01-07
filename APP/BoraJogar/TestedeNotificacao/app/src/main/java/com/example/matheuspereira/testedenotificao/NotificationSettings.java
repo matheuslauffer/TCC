3f4e1eacf334ec8d7ef075eec71069d41bfb4b71
matheuslauffer <matheuslauffer1@gmail.com>
2019-01-07 17:20:14 -0200

    Criação do receptor de noptificação

---
+++ APP/BoraJogar/TestedeNotificacao/app/src/main/java/com/example/matheuspereira/testedenotificao/NotificationSettings.java
@@ -0,0 +1,7 @@
+package com.example.matheuspereira.testedenotificao;
+
+public class NotificationSettings {
+    public static String SenderId = "AIzaSyANllQbSjkhxDKZpL4sY9yxjVyMdOr6RfY";
+    public static String HubName = "DeteccaoDeQuedasNotificacao";
+    public static String HubListenConnectionString = "Endpoint=sb://deteccaodequedas.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=UYahXlIcp1Tilj6EdzfVfajPUkRMyZqnrTZsq5uQXf0=";
+}
